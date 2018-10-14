eb69a8867e4b5d9beea819e3b82789c2d5bb81c3
Huynh Nguyen <huynh.nguyen2207@gmail.com>
2018-10-14 13:30:36 +0700

    fdvbdbd

--- js/app.js
+++ js/app.js
@@ -1,108 +1,138 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
   $('.banner').slick({
+    arrows: false,
     autoplay: true,
     autoplaySpeed: 2000,
-    arrows: false,
   });
 
-  $('.nav-bar').sticky();
-});
+  $(function() {
+    $.scrollify({
+      section: ".content-block",
+      interstitialSection : "header, footer",
+      standardScrollElements: ".service"
+    });
+  });
 
-$(window).scroll(function () {
-    var sc = $(window).scrollTop()
-    if (sc > 100) {
-        $(".nav-bar").addClass("bg");
-        $(".logo-header-desktop").hide();
-        $(".logo-header-mobile").show();
-    } else {
-        $(".nav-bar").removeClass("bg");
-        $(".logo-header-desktop").show();
-        $(".logo-header-mobile").hide();
+  if($(window).width() <= 768) {
+    $.scrollify.setOptions({
+      standardScrollElements: ".service-block",
+    })
+  };
+
+  // Civil Engineer Modal Box
+  $("#open-civil-engineer-mobile").click(function() {
+    event.preventDefault();
+    $("#modal-civil-engineer-mobile").css("display","block");
+    $.scrollify.disable();
+  });
+
+  $("#close-civil-engineer-mobile").click(function() {
+    $("#modal-civil-engineer-mobile").css("display","none");
+    $.scrollify.enable();
+  });
+
+
+  // Bridge Engineer Modal Box
+  $("#open-bridge-engineer-mobile").click(function() {
+    event.preventDefault();
+    $("#modal-bridge-engineer-mobile").css("display","block");
+    $.scrollify.disable();
+  });
+
+  $("#close-bridge-engineer-mobile").click(function() {
+    $("#modal-bridge-engineer-mobile").css("display","none");
+    $.scrollify.enable();
+  });
+
+  // Junior Engineer Modal Box
+  $("#open-junior-engineer-mobile").click(function() {
+    event.preventDefault();
+    $("#modal-junior-engineer-mobile").css("display","block");
+    $.scrollify.disable();
+  });
+
+  $("#close-junior-engineer-mobile").click(function() {
+    $("#modal-junior-engineer-mobile").css("display","none");
+    $.scrollify.enable();
+  });
+
+  ////////////////////////////////////////////
+
+  // Civil Engineer Modal Box
+  $("#open-civil-engineer-desktop").click(function() {
+    event.preventDefault();
+    $("#modal-civil-engineer-desktop").css("display","block");
+    $.scrollify.disable();
+  });
+
+  $("#close-civil-engineer-desktop").click(function() {
+    $("#modal-civil-engineer-desktop").css("display","none");
+    $.scrollify.enable();
+  });
+
+
+  // Bridge Engineer Modal Box
+  $("#open-bridge-engineer-desktop").click(function() {
+    event.preventDefault();
+    $("#modal-bridge-engineer-desktop").css("display","block");
+    $.scrollify.disable();
+  });
+
+  $("#close-bridge-engineer-desktop").click(function() {
+    $("#modal-bridge-engineer-desktop").css("display","none");
+    $.scrollify.enable();
+  });
+
+  // Junior Engineer Modal Box
+  $("#open-junior-engineer-desktop").click(function() {
+    event.preventDefault();
+    $("#modal-junior-engineer-desktop").css("display","block");
+    $.scrollify.disable();
+  });
+
+  $("#close-junior-engineer-desktop").click(function() {
+    $("#modal-junior-engineer-desktop").css("display","none");
+    $.scrollify.enable();
+  });
+
+  // Change .nav-bar style when scrolled
+  $(window).scroll(function () {
+        var sc = $(window).scrollTop()
+        if (sc > 100) {
+            $(".nav-bar").css("padding", "15px 0");
+            $(".nav-bar").css("border-bottom", "4px solid #3CC5C8");
+        } else {
+            $(".nav-bar").css("padding", "30px 0");
+            $(".nav-bar").css("border-bottom", "none");
+        }
+    });
+
+    if ($(window).width() > 768) {
+      $("#overlay").removeClass("overlay");
+      $("#overlay").addClass("nav");
+
+      $(window).scroll(function () {
+            var sc = $(window).scrollTop()
+            if (sc > 100) {
+                $(".nav-bar").addClass("scrolled-nav");
+            } else {
+                $(".nav-bar").removeClass("scrolled-nav");
+            }
+        });
+
+      $("main.content-mobile").remove();
+
+      $(".btn-banner-career").attr("href", "#career-desktop");
+    }
+
+    if($(window).width() <= 768) {
+      $("main.content-desktop").remove();
     }
 });
 
-
-
-// Civil Engineer Modal
-// Get the modal
-var civilModal = document.getElementById('modal-civil');
-
-// Get the button that opens the modal
-var civilBtn = document.getElementById("btn-civil");
-
-// Get the <span> element that closes the modal
-var civilSpan = document.getElementsByClassName("close-civil")[0];
-
-// When the user clicks on the button, open the modal
-civilBtn.onclick = function() {
-  event.preventDefault();
-  civilModal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-civilSpan.onclick = function() {
-  civilModal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == civilModal) {
-    civilModal.style.display = "none";
-  }
-}
-
-// Bridge Engineer Modal
-// Get the modal
-var bridgeModal = document.getElementById('modal-bridge');
-
-// Get the button that opens the modal
-var bridgeBtn = document.getElementById("btn-bridge");
-
-// Get the <span> element that closes the modal
-var bridgeSpan = document.getElementsByClassName("close-bridge")[0];
-
-// When the user clicks on the button, open the modal
-bridgeBtn.onclick = function() {
-  event.preventDefault();
-  bridgeModal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-bridgeSpan.onclick = function() {
-  bridgeModal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == bridgeModal) {
-    bridgeModal.style.display = "none";
-  }
-}
-
-// Junior Engineer Modal
-// Get the modal
-var juniorModal = document.getElementById('modal-junior');
-
-// Get the button that opens the modal
-var juniorBtn = document.getElementById("btn-junior");
-
-// Get the <span> element that closes the modal
-var juniorSpan = document.getElementsByClassName("close-junior")[0];
-
-// When the user clicks on the button, open the modal
-juniorBtn.onclick = function() {
-  event.preventDefault();
-  juniorModal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-juniorSpan.onclick = function() {
-  juniorModal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == juniorModal) {
-    juniorModal.style.display = "none";
-  }
-}
+//refresh page on browser resize
+$(window).bind('resize', function(e) {
+  this.location.reload(false); /* false to get page from cache */
+  /* true to fetch page from server */
+});
